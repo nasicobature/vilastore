0b6ba4522531c761352196ef4095fa95aab4ec85
nasicobature <nasicobature@users.noreply.github.com>
2026-03-15 05:22:01 -0700

    Revert "Update build and render configuration"
    This reverts commit ae592fe75c50464c74e476cb621c155e4bea73f6.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
 mkdir -p "${MEDIA_ROOT:-media}"
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py check_static_assets
 python manage.py migrate
