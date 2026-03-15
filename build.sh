ae592fe75c50464c74e476cb621c155e4bea73f6
nasicobature <nasicobature@users.noreply.github.com>
2026-03-15 04:50:06 -0700

    Update build and render configuration

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-mkdir -p "${MEDIA_ROOT:-media}"
 pip install -r requirements.txt
+mkdir -p "${MEDIA_ROOT:-media}"
 python manage.py collectstatic --no-input
 python manage.py check_static_assets
 python manage.py migrate
