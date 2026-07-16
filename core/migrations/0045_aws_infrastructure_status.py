92c6880a1b7e58aa464a8a5e05a23a71b629ea5d
nasicobature <nasicobature@users.noreply.github.com>
2026-07-16 16:52:29 -0700

    Add no-op AWS migration compatibility

---
+++ core/migrations/0045_aws_infrastructure_status.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0044_widen_sale_amount_fields"),
+    ]
+
+    operations = []
